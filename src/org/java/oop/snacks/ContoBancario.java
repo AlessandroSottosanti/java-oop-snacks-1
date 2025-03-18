069a21533badef581085a13168e828499bf27fcd
AlessandroSottosanti <asottosanti2@gmail.com>
2025-03-18 16:57:37 +0100

    feat: snack 2

--- src/org/java/oop/snacks/ContoBancario.java
+++ src/org/java/oop/snacks/ContoBancario.java
@@ -1,5 +1,34 @@
 package org.java.oop.snacks;
 
+import java.math.BigDecimal;
+
 public class ContoBancario {
+
+    private int numeroDiConto;
+    private BigDecimal saldo;
+
+    public ContoBancario(int numeroDiConto) {
+        this.numeroDiConto = numeroDiConto;
+        this.saldo = new BigDecimal(0);
+    }
+
+    public int getNumeroDiConto() {
+        return this.numeroDiConto;
+    }
+
+    // Mostra saldo
+    public BigDecimal getSaldo() {
+        return this.saldo;
+    }
+
+    // preleva
+    public void preleva(BigDecimal prelievo) {
+        this.saldo = this.saldo.subtract(prelievo);
+    }
+
+    // deposita
+    public void deposita(BigDecimal deposito) {
+        this.saldo = this.saldo.add(deposito);
+    }
 
 }
--- src/org/java/oop/snacks/run.java
+++ src/org/java/oop/snacks/run.java
@@ -1,7 +1,11 @@
 package org.java.oop.snacks;
 
+import java.math.BigDecimal;
+import java.util.Random;
+
 public class run {
     public static void main(String[] args) {
+    Random random = new Random();
 
         // SNACK 1
 
@@ -10,10 +14,36 @@ public class run {
 
         // output
         System.out.println("");
+        System.out.println("Snack 1");
 
         System.out.println("Studente:");
         System.out.println(Studente.getStudente(carlo.getNome(), carlo.getCognome(), carlo.getEta()));
 
         System.out.println("");
+
+
+        // SNACK 2
+
+        // inizializzazione conto tramite costruttore
+        ContoBancario conto = new ContoBancario(random.nextInt(99999));
+
+        System.out.println("Snack 2");
+
+        System.out.println("Numero di conto: " + conto.getNumeroDiConto() + ", Saldo: " + conto.getSaldo() + "$");
+
+        System.out.println("");
+
+        // Deposita
+
+        conto.deposita(new BigDecimal(60));
+
+        System.out.println("Deposita 60$ = " + "Saldo: " + conto.getSaldo() + "$");
+
+        // Preleva
+
+        conto.preleva(new BigDecimal(20));
+
+        System.out.println("Preleva 20$ = " + "Saldo: " + conto.getSaldo() + "$");
+
     }
 }
